9c00c352a357829ec810d3118afad65a1bc9a638
Carlos Montaño <carlos.montano@paynalli.mx>
2024-09-26 21:58:43 -0700

    initial commit

---
+++ src/test/java/com/beehive/beehive_nest/BeehiveNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beehive.beehive_nest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeehiveNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
