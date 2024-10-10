149625804f015ccfc00a80e1908bc3de7bbb0db3
Carlos Montaño <montanoc70@gmail.com>
2024-10-09 23:34:30 -0700

    added user role crud

---
+++ src/main/java/com/beehive/beehiveNest/model/forms/UserRoleForm.java
@@ -0,0 +1,14 @@
+package com.beehive.beehiveNest.model.forms;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+public class UserRoleForm {
+    private String description;
+}
